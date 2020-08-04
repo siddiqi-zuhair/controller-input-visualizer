69837e413a5b823c2a558ef0f706f9923a61e06b
Chujji <siddiqizuhair@gmail.com>
2020-08-04 15:44:43 -0400

    Added customizable backgrounds

--- src/main/java/BoardPanel.java
+++ src/main/java/BoardPanel.java
@@ -4,100 +4,102 @@ import java.net.MalformedURLException;
 import java.net.URL;
 
 public class BoardPanel extends JPanel {
-    ImageIcon controller = new ImageIcon();
-    ImageIcon leftbumper = new ImageIcon();
-    ImageIcon rightbumper = new ImageIcon();
+    public static ImageIcon xboxController = new ImageIcon();
+    public static ImageIcon xboxLeftbumper = new ImageIcon();
+    public static ImageIcon xboxRightbumper = new ImageIcon();
     public void paintComponent (Graphics g) {
         super.paintComponent(g);
 
-        g.drawImage(controller.getImage(), 0, 0, null);
+        g.drawImage(xboxController.getImage(), 0, 10, null);
         g.setColor(Color.BLACK);
 
-        g.fillOval((int) (Main.leftx * 25 + 100), (int) (Main.lefty * 25 + 115), 5, 5);
-        g.fillOval((int) (Main.rightx * 25 + 320), (int) (Main.righty * 25 + 200), 5, 5);
+        g.fillOval((int) (Main.leftx * 25 + 100), (int) (Main.lefty * 25 + 115+10), 5, 5);
+        g.fillOval((int) (Main.rightx * 25 + 320), (int) (Main.righty * 25 + 200+10), 5, 5);
 
-        g.fillRect(75, 0, (int) (Main.ltrigger * 85), 15);
-        g.fillRect(410, 0, -(int) (Main.rtrigger * 85), 15);
+        g.fillRect(75, 0+10, (int) (Main.ltrigger * 85), 15);
+        g.fillRect(410, 0+10, -(int) (Main.rtrigger * 85), 15);
 
         if (Main.controller.getPov(0).toString().equalsIgnoreCase("north")) {
-            g.fillRect(167, 165, 27, 30);
+            g.fillRect(167, 165+10, 27, 30);
         } else if (Main.controller.getPov(0).toString().equalsIgnoreCase("south")) {
-            g.fillRect(167, 220, 27, 30);
+            g.fillRect(167, 220+10, 27, 30);
         } else if (Main.controller.getPov(0).toString().equalsIgnoreCase("east")) {
-            g.fillRect(197, 195, 27, 30);
+            g.fillRect(197, 195+10, 27, 30);
         } else if (Main.controller.getPov(0).toString().equalsIgnoreCase("west")) {
-            g.fillRect(140, 195, 27, 30);
+            g.fillRect(140, 195+10, 27, 30);
         } else if (Main.controller.getPov(0).toString().equalsIgnoreCase("northWest")) {
-            g.fillRect(140, 195, 27, 30);
-            g.fillRect(167, 165, 27, 30);
+            g.fillRect(140, 195+10, 27, 30);
+            g.fillRect(167, 165+10, 27, 30);
         } else if (Main.controller.getPov(0).toString().equalsIgnoreCase("southWest")) {
-            g.fillRect(167, 220, 27, 30);
-            g.fillRect(140, 195, 27, 30);
+            g.fillRect(167, 220+10, 27, 30);
+            g.fillRect(140, 195+10, 27, 30);
         } else if (Main.controller.getPov(0).toString().equalsIgnoreCase("southEast")) {
-            g.fillRect(197, 195, 27, 30);
-            g.fillRect(167, 220, 27, 30);
+            g.fillRect(197, 195+10, 27, 30);
+            g.fillRect(167, 220+10, 27, 30);
         } else if (Main.controller.getPov(0).toString().equalsIgnoreCase("northEast")) {
-            g.fillRect(167, 165, 27, 30);
-            g.fillRect(197, 195, 27, 30);
+            g.fillRect(167, 165+10, 27, 30);
+            g.fillRect(197, 195+10, 27, 30);
         }
         if (Main.controller.getButton(0)) {
-            g.fillOval(367, 143, 30, 30);
+            g.fillOval(367, 143+10, 30, 30);
             g.setColor(Color.GREEN);
-            g.drawString("A",379,162);
+            g.drawString("A",379,162+10);
             g.setColor(Color.BLACK);
         }
         if (Main.controller.getButton(1)) {
-            g.fillOval(400, 110, 30, 30);
+            g.fillOval(400, 110+10, 30, 30);
             g.setColor(Color.RED);
-            g.drawString("B",413,129);
+            g.drawString("B",413,129+10);
             g.setColor(Color.BLACK);
 
         }
         if (Main.controller.getButton(2)) {
-            g.fillOval(333, 110, 30, 30);
+            g.fillOval(333, 110+10, 30, 30);
             g.setColor(Color.CYAN);
-            g.drawString("X",346,129);
+            g.drawString("X",346,129+10);
             g.setColor(Color.BLACK);
         }
         if (Main.controller.getButton(3)) {
-            g.fillOval(368, 74, 30, 30);
+            g.fillOval(368, 74+10, 30, 30);
             g.setColor(Color.YELLOW);
-            g.drawString("Y",379,93);
+            g.drawString("Y",379,10+93);
             g.setColor(Color.BLACK);
         }
         if (Main.controller.getButton(4)) {
-            g.fillOval(195 - 9, 158 - 43, 20, 20);
+            g.fillOval(195 - 9, 158 - 43+10, 20, 20);
         }
         if (Main.controller.getButton(6)) {
-            g.fillOval(279 - 9, 158 - 43, 20, 20);
+            g.fillOval(279 - 9, 158 - 43+10, 20, 20);
         }
         if (Main.controller.getButton(7)) {
-            g.fillOval(166 - 9, 330 - 43, 50, 50);
+            g.fillOval(166 - 9, 330 - 43+10, 50, 50);
             g.setColor(Color.WHITE);
-            g.drawString("LS",183-9,360-43);
+            g.drawString("LS",183-9,360-43+10);
         }
         if (Main.controller.getButton(8)) {
             g.setColor(Color.BLACK);
-            g.fillOval(306 - 9, 330 - 43, 50, 50);
+            g.fillOval(306 - 9, 330 - 43+10, 50, 50);
             g.setColor(Color.WHITE);
-            g.drawString("RS", 324 - 9, 360 - 43);
+            g.drawString("RS", 324 - 9, 360 - 43+10);
         }
         if(Main.controller.getButton(9)){
-            g.drawImage(leftbumper.getImage(), 62-8, 49-33, null);
+            g.drawImage(xboxLeftbumper.getImage(), 62-8, 49-33+10, null);
         }
         if(Main.controller.getButton(10)){
-            g.drawImage(rightbumper.getImage(), 305-8, 49-33, null);
+            g.drawImage(xboxRightbumper.getImage(), 305-8, 49-33+10, null);
         }
 
         }
     BoardPanel(){
         super();
         try {
-            controller = new ImageIcon(new URL("https://i.imgur.com/ebt6Rxk.png"));
-            leftbumper = new ImageIcon(new URL("https://i.imgur.com/4pMtOND.png"));
-            rightbumper = new ImageIcon(new URL("https://i.imgur.com/oInlRrI.png"));
+            xboxController = new ImageIcon(new URL("https://i.imgur.com/ebt6Rxk.png"));
+            xboxLeftbumper = new ImageIcon(new URL("https://i.imgur.com/4pMtOND.png"));
+            xboxRightbumper = new ImageIcon(new URL("https://i.imgur.com/oInlRrI.png"));
         }catch(MalformedURLException e) {
         }
-        }
+        Main.frame.setIconImage(BoardPanel.xboxController.getImage());
+
+    }
 
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -17,12 +17,16 @@ import java.lang.reflect.Method;
 import java.util.Arrays;
 
 
-public class Main {
-    public static JFrame frame = new JFrame("S Input Display");
+public class Main extends Component implements ActionListener {
+    public static JFrame frame = new JFrame("Gamepad Input Display");
     public static BoardPanel panel = new BoardPanel();
+    public static JMenuBar optionBar  = new JMenuBar();
+    public static JMenu optionsMenu = new JMenu("Options");
+    public static JMenuItem colorChooser = new JMenuItem("Color Chooser");
+
     public static SDL2ControllerManager controllerManager;
-    public static boolean loop = true;
     public static SDL2Controller controller;
+
     public static float leftx;
     public static float lefty;
     public static float rightx;
@@ -30,51 +34,76 @@ public class Main {
     public static float ltrigger;
     public static float rtrigger;
 
-    private static void init() {
-        try {
-            controllerManager = new SDL2ControllerManager();
-            controller = (SDL2Controller) controllerManager.getControllers().get(0);
-            while (loop) {
+    Color initColor = Color.WHITE;
+    Color bgColor = Color.WHITE;
+
+    public static String controllerType;
+    public Timer time = new Timer(1000/60, this);
+
+    public  void actionPerformed(ActionEvent evt) {
+        if (evt.getSource() == time) {
+            try {
+                System.out.println(controllerType);
                 PointerInfo a = MouseInfo.getPointerInfo();
                 Point b = a.getLocation();
 
                 double mousex = b.getX();
                 double mousey = b.getY();
-            //    System.out.println("Mouse X: " + mousex);
-              //  System.out.println("Mouse Y: " +mousey);
+                //    System.out.println("Mouse X: " + mousex);
+                //  System.out.println("Mouse Y: " +mousey);
                 controllerManager.pollState();
                 String strPower = controller.getPowerLevel().toString();
+                System.out.println(controller.getPlayerIndex());
+                ;
                 //System.out.println(strPower);
-                 leftx = controller.getAxis(0);
-                 lefty = controller.getAxis(1);
-                 rightx = controller.getAxis(2);
-                 righty = controller.getAxis(3);
-                 ltrigger = controller.getAxis(4);
-                 rtrigger = controller.getAxis(5);
+                leftx = controller.getAxis(0);
+                lefty = controller.getAxis(1);
+                rightx = controller.getAxis(2);
+                righty = controller.getAxis(3);
+                ltrigger = controller.getAxis(4);
+                rtrigger = controller.getAxis(5);
                 //System.out.println(ltrigger);
-           //     System.out.println("Left X: " +leftx);
-              //  System.out.println("Left Y: "+lefty);
+                //     System.out.println("Left X: " +leftx);
+                //  System.out.println("Left Y: "+lefty);
                 //System.out.println("Right X: "+rightx);
-               // System.out.println("Right Y: "+righty);
-               // System.out.println(controller.getPov(0));
+                // System.out.println("Right Y: "+righty);
+                // System.out.println(controller.getPov(0));
                 panel.repaint();
+            } catch (SDL_Error e) {
             }
-        }catch(SDL_Error e) {
         }
+        if (evt.getSource() == colorChooser) {
+            bgColor = JColorChooser.showDialog(this, "Pick a background color", initColor);
+            panel.setBackground(bgColor);
         }
-    public static void main(String[] args) {
+    }
+        public Main(){
+                panel.setPreferredSize(new Dimension(495,400));
+                panel.setLayout(null);
+                optionBar.setLocation(0,0);
+                optionBar.setSize(495,15);
+                optionBar.add(optionsMenu);
+                optionsMenu.add(colorChooser);
+                panel.add(optionBar);
+                colorChooser.addActionListener(this);
 
-        panel.setPreferredSize(new Dimension(495,370));
+                frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+                frame.setContentPane(panel);
+                frame.pack();
 
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setContentPane(panel);
-        frame.pack();
+                frame.setResizable(false);
+                frame.setVisible(true);
+                panel.repaint();
+                controllerManager = new SDL2ControllerManager(SDL2ControllerManager.InputPreference.XINPUT);
+                controller = (SDL2Controller) controllerManager.getControllers().get(0);
+                controllerType = controller.getType().toString();
+                time.start();
 
-        frame.setResizable(false);
-        panel.setBackground(Color.WHITE);
-        frame.setVisible(true);
-        panel.repaint();
-        init();
+            }
+    public static void main(String[] args) throws ClassNotFoundException, UnsupportedLookAndFeelException, InstantiationException, IllegalAccessException {
+        UIManager.setLookAndFeel( UIManager.getSystemLookAndFeelClassName()); //setLookandFeel for modern Windows 10 look on buttons and other JComponents
+
+        new Main();
 
 
 
